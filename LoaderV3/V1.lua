898361b10aa8888727c5a838cb172b92b5bc98fb
Christian Leocario <92495274+nchnnnn@users.noreply.github.com>
2025-07-02 21:51:29 +0800

    Update V1.lua

--- LoaderV3/V1.lua
+++ LoaderV3/V1.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 local scripts = {
-    [126884695634066] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/Garden-V2.lua",
+    [126884695634066] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/V0.lua",
 }
 
 local url = scripts[game.PlaceId]
